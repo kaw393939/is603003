09fa670f8f7379142e42ca8433ef2c4cea59b113
Keith Williams <keith@webizly.com>
2017-10-05 13:30:12 -0400

    I am demonstrating git integration

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  
+  $class = 'IS 601';
+
+  echo 'I\'m going to ' .  $class . '</br>';
+  echo "hello world $class </br>";
+
+  $a = 1;
+  $b = 2;
+  $c = $a + $b;
+
+  echo $a . '+' . $b . '=' . $c;
+  echo 'stuff';
+
+?>
